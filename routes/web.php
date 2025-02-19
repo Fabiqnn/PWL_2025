e723527fe827b59acbb4090a448ad970ac2b1b00
Fabiqnn <arema20056@gmail.com>
2025-02-19 09:32:09 +0700

    Pertemuan 1_Percobaan 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+    return 'hello world';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Nim : 2341270170 <br> Nama : Fabian Hasbilllah';
 });
