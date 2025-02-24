02641893b32228871f84f9fdc7117fa2675f240a
Fabiqnn <arema20056@gmail.com>
2025-02-25 05:44:46 +0700

    Pertemuan 2_percobaan 7 part 1

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,12 @@ Route::get('/user/profile' , function () {
 //     Route::get('/event', [EventController::class, 'index']);
 // });
 
+// percobaan 6
 use App\Http\Controllers\PhotoController;
 Route::resource('photos', PhotoController::class)->only(['index', 'show']);
 Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
+
+// percobaan 7
+Route::get('/greeting', function () {
+    return view('hello', ['name' => 'Fabian Hasbillah']);
+});
